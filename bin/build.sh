2e60a21fa410102fc86df91ee579d13b3da8dbd4
Junichi Kato <junichi.kato@gree.net>
2014-06-14 11:11:55 +0900

    refactor

--- bin/build.sh
+++ bin/build.sh
@@ -2,12 +2,12 @@
 
 . ./env.sh
 
-sudo docker build --no-cache=true -t $NAME .
+docker build --no-cache=true -t $NAME .
 
 reimport() {
   CID=$(sudo docker run -d $NAME /bin/bash -c "")
-  sudo docker export $CID > tmp.tar
-  sudo docker kill $CID
+  docker export $CID > tmp.tar
+  docker kill $CID
   cat tmp.tar | sudo docker import - base
   rm -f tmp.tar
 }
