305a16aa48a8c13ee7d387561f2f045f8587b5ed
じゅんいち☆かとう <j5ik2o@gmail.com>
2014-06-15 11:16:00 +0900

    Refactor

---
+++ crowd/bash.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+. ./env.sh
+
+docker run -i -t "j5ik2o/$NAME" /bin/bash
---
+++ crowd/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+. ./env.sh
+
+docker build -t "j5ik2o/$NAME" .
+#docker build --no-cache=true -t "j5ik2o/$NAME" .
+
+reimport() {
+  CID=$(sudo docker run -d $NAME /bin/bash -c "")
+  docker export $CID > tmp.tar
+  docker kill $CID
+  cat tmp.tar | sudo docker import - base
+  rm -f tmp.tar
+}
+
+#reimport
