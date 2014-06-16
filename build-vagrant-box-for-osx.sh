3eb3a4c4648b67685fda310153985ce1e46fb8b3
じゅんいち☆かとう <j5ik2o@gmail.com>
2014-06-16 15:06:33 +0900

    Added a build script to vagrant box.

---
+++ build-vagrant-box-for-osx.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+brew tap homebrew/einary
+brew install packer
+
+mkdir -p temp
+cd temp
+git clone https://github.com/momijiame/packer-vanilla-box.git
+
+cd packer-vanilla-box/ubuntu
+packer build ubuntu1404.json
+
+cd artifacts
+vagrant box add ubuntu1404 ubuntu1404-virtualbox.box
+
+
