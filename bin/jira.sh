579ef3f29c3b8722b4e737a09de213d311a8e927
Junichi Kato <j5ik2o@gmail.com>
2014-06-17 01:43:41 +0900

    refactor

--- bin/jira.sh
+++ bin/jira.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
-sudo docker run -d -v /var/log/atlassian/jira:/opt/apache-tomcat/logs \
+start(){
+sudo docker run -d --name jira -v /var/log/atlassian/jira:/opt/apache-tomcat/logs \
 -v /var/lib/atlassian/jira:/var/lib/atlassian/jira \
 -p 8081:8080 -t j5ik2o/jira
+}
+
+stop() {
+CID=`sudo docker ps | grep jira | awk '{ print $1 }'`
+sudo docker stop $CID
+}
+
+case "$1" in
+start)
+	start
+	;;
+stop)
+	stop
+	;;
+esac
+
--- bin/mysql.sh
+++ bin/mysql.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
- 
-sudo docker run -d -v /var/lib/mysql:/var/lib/mysql -v /var/log/mysql:/var/log/mysql -p 3306:3306 -t j5ik2o/mysql
+
+start() {
+sudo docker run -d --name mysql -v /var/lib/mysql:/var/lib/mysql -v /var/log/mysql:/var/log/mysql -p 3306:3306 -t j5ik2o/mysql
+}
+
+stop() {
+CID=`sudo docker ps | grep mysql | awk '{ print $1 }'`
+sudo docker stop $CID
+}
+
+case "$1" in
+start) 
+	start
+	;;
+stop)
+	stop
+	;;
+esac
+
